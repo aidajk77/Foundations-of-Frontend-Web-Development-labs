ad24c41cd778c90b66dcee03a963d989f02cde8b
aidajk77 <aidajkunic@gmail.com>
2024-03-28 21:38:13 +0100

    final

--- Project1/script.js
+++ Project1/script.js
@@ -4,3 +4,14 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+function openCvModal() {
+    // Show the Bootstrap modal
+    const cvModal = new bootstrap.Modal(document.getElementById('cvModal'));
+    cvModal.show();
+}
+
+// Select the Download CV button
+const downloadCvBtn = document.getElementById('downloadCvBtn');
+
+// Add click event listener to the button and call the function
+downloadCvBtn.addEventListener('click', openCvModal);
